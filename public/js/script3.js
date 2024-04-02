d3d147b32618626c841bb55f1898a3993401796a
Tony Santos Costa <33641618+costatony79@users.noreply.github.com>
2024-04-02 11:22:51 -0400

    Finalização Atividade Geogrfaia

--- public/js/script3.js
+++ public/js/script3.js
@@ -1,7 +1,7 @@
-const estadosBrasileiros = ["ACRE", "ALAGOAS", "AMAPÁ", "AMAZONAS", "BAHIA", "CEARÁ", "DISTRITO FEDERAL", "ESPÍRITO SANTO", "GOIÁS", "MARANHÃO", "MATO GROSSO", "MATO GROSSO DO SUL", "MINAS GERAIS", "PARÁ", "PARAÍBA", "PARANÁ", "PERNAMBUCO", "PIAUÍ", "RIO DE JANEIRO", "RIO GRANDE DO NORTE", "RIO GRANDE DO SUL", "RONDÔNIA", "RORAIMA", "SANTA CATARINA", "SÃO PAULO", "SERGIPE", "TOCANTINS"];
-const capitaisEstaduais = ["RIO BRANCO", "MACEIÓ", "MACAPÁ", "MANAUS", "SALVADOR", "FORTALEZA", "BRASÍLIA", "VITÓRIA", "GOIÂNIA", "SÃO LUÍS", "CUIABÁ", "CAMPO GRANDE", "BELO HORIZONTE", "BELÉM", "JOÃO PESSOA", "CURITIBA", "RECIFE", "TERESINA", "RIO DE JANEIRO", "NATAL", "PORTO ALEGRE", "PORTO VELHO", "BOA VISTA", "FLORIANÓPOLIS", "SÃO PAULO", "ARACAJU", "PALMAS"];
+const estadosBrasileiros = ["ACRE", "ALAGOAS", "AMAPÁ", "AMAZONAS", "BAHIA", "CEARÁ", "DISTRITOFEDERAL", "ESPÍRITOSANTO", "GOIÁS", "MARANHÃO", "MATOGROSSO", "MATOGROSSODOSUL", "MINASGERAIS", "PARÁ", "PARAÍBA", "PARANÁ", "PERNAMBUCO", "PIAUÍ", "RIODEJANEIRO", "RIOGRANDEDONORTE", "RIOGRANDEDOSUL", "RONDÔNIA", "RORAIMA", "SANTACATARINA", "SÃOPAULO", "SERGIPE", "TOCANTINS"];
+const capitaisEstaduais = ["RIOBRANCO", "MACEIÓ", "MACAPÁ", "MANAUS", "SALVADOR", "FORTALEZA", "BRASÍLIA", "VITÓRIA", "GOIÂNIA", "SÃOLUÍS", "CUIABÁ", "CAMPOGRANDE", "BELOHORIZONTE", "BELÉM", "JOÃOPESSOA", "CURITIBA", "RECIFE", "TERESINA", "RIODEJANEIRO", "NATAL", "PORTOALEGRE", "PORTOVELHO", "BOAVISTA", "FLORIANÓPOLIS", "SÃOPAULO", "ARACAJU", "PALMAS"];
 const siglasEstaduais = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"];
-const regioesEstaduais = ["NORTE", "NORDESTE", "NORTE", "NORTE", "NORDESTE", "NORDESTE", "CENTRO-OESTE", "SUDESTE", "CENTRO-OESTE", "NORDESTE", "CENTRO-OESTE", "CENTRO-OESTE", "SUDESTE", "NORTE", "NORDESTE", "SUL", "NORDESTE", "NORDESTE", "SUDESTE", "NORDESTE", "SUL", "NORTE", "NORTE", "SUL", "SUDESTE", "NORDESTE", "NORTE"];
+const regioesEstaduais = ["NORTE", "NORDESTE", "NORTE", "NORTE", "NORDESTE", "NORDESTE", "CENTROOESTE", "SUDESTE", "CENTROOESTE", "NORDESTE", "CENTROOESTE", "CENTROOESTE", "SUDESTE", "NORTE", "NORDESTE", "SUL", "NORDESTE", "NORDESTE", "SUDESTE", "NORDESTE", "SUL", "NORTE", "NORTE", "SUL", "SUDESTE", "NORDESTE", "NORTE"];
 
 const estados = document.querySelectorAll('.estado');
 const capitais = document.querySelectorAll('.capital');
@@ -42,4 +42,76 @@ function verificaResposta(){
         }
       }
       inputEstados = [];
+      capitais.forEach(campo => {
+        const valor = campo.value.toUpperCase().replace(/\s/g, '');
+        if (valor.trim() !== '') {
+            inputCapitais.push(valor);
+        }
+      });
+
+      console.log(inputCapitais);
+
+      for(i = 0; i < inputCapitais.length; i++){
+        console.log(inputCapitais[i]);
+        if(inputCapitais[i] === capitaisEstaduais[i] ){            
+           console.log("Entrou");
+           entrou = true;
+                if(entrou){
+                    capitais[i].disabled = true;  
+                    capitais[i].classList.add("correto");
+                }
+        } else {
+            capitais[i].classList.add("incorreto");
+            console.log("Não Entrou");
+        }
+      }
+      inputCapitais = [];
+      siglas.forEach(campo => {
+        const valor = campo.value.toUpperCase().replace(/\s/g, '');
+        if (valor.trim() !== '') {
+            inputSiglas.push(valor);
+        }
+      });
+
+      console.log(inputSiglas);
+
+      for(i = 0; i < inputSiglas.length; i++){
+        console.log(inputSiglas[i]);
+        if(inputSiglas[i] === siglasEstaduais[i] ){            
+           console.log("Entrou");
+           entrou = true;
+                if(entrou){
+                    siglas[i].disabled = true;  
+                    siglas[i].classList.add("correto");
+                }
+        } else {
+            siglas[i].classList.add("incorreto");
+            console.log("Não Entrou");
+        }
+      }
+      inputSiglas = [];
+      regioes.forEach(campo => {
+        const valor = campo.value.toUpperCase().replace(/\s/g, '');
+        if (valor.trim() !== '') {
+            inputRegioes.push(valor);
+        }
+      });
+
+      console.log(inputRegioes);
+
+      for(i = 0; i < inputRegioes.length; i++){
+        console.log(inputRegioes[i]);
+        if(inputRegioes[i] === regioesEstaduais[i] ){            
+           console.log("Entrou");
+           entrou = true;
+                if(entrou){
+                    regioes[i].disabled = true;  
+                    regioes[i].classList.add("correto");
+                }
+        } else {
+            regioes[i].classList.add("incorreto");
+            console.log("Não Entrou");
+        }
+      }
+      inputRegioes = [];
 }
